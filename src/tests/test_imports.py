05a99adc05e86964a58e2517568a5ea28ea119ca
Murtaza Hussain <murtaza32004@gmail.com>
2026-04-19 18:46:50 -0700

    Add additional imports to test_imports.py

--- src/tests/test_imports.py
+++ src/tests/test_imports.py
@@ -4,3 +4,10 @@ def test_imports():
     import src.models.model
     import src.models.train
     import src.models.evaluate
+
+    import src.inference.predict
+    import src.inference.webcam
+
+    import api.app
+
+    import src.utils.mlflow_config
