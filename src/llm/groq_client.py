f9bdeff5b52cbb272b360c93c950342874bfd3f8
Murtaza Hussain <murtaza32004@gmail.com>
2026-05-15 04:26:27 -0700

    Update groq_client.py

--- src/llm/groq_client.py
+++ src/llm/groq_client.py
@@ -21,3 +21,17 @@ def explain_emotion(emotion, confidence):
     Also if the user talks about something other than emotion
     just continute the conversation but under 40 tokens.
     """
+
+    try:
+
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            temperature=0.7,
+            max_tokens=80
+        )
